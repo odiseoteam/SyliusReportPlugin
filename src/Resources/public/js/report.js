8c45e185f285af76e3209554d4113cfa49c24d27
Damian Damico <damian@odiseo.com.ar>
2019-06-06 11:00:56 -0300

    create setup autocomplete

--- src/Resources/public/js/report.js
+++ src/Resources/public/js/report.js
@@ -13,42 +13,91 @@ var handleTimePeriodEndTodayCheckboxChange = function () {
     $timePeriodEndToday.closest('.field').find('select').prop('disabled', isChecked);
 };
 
-var setupForms = function () {
-    $('#odiseo_sylius_report_dataFetcher_configuration .ui.toggle.checkbox').checkbox();
-    $('#odiseo_sylius_report_dataFetcher_configuration .ui.dropdown').dropdown();
-    $('.sylius-autocomplete').autoComplete();
-    /*$('.ui.dropdown.remote.search').each(function (index, dropdown) {
-        var $dropdown = $(dropdown);
-        var url = $dropdown.find('select').data('remoteUrl');
+var setupAutocomplete = function () {
+    $('.sylius-autocomplete').each(function(idx, el) {
+        var element = $(el);
+        var criteriaName = element
+            .data('criteria-name');
+        var choiceName = element
+            .data('choice-name');
+        var choiceValue = element
+            .data('choice-value');
+        var autocompleteValue = element
+            .find('input.autocomplete')
+            .val();
+        var loadForEditUrl = element
+            .data('load-edit-url');
 
-        $dropdown.dropdown({
-            delay: {
-                search: 250
-            },
-            forceSelection: false,
-            apiSettings: {
-                url: url,
-                dataType: 'JSON',
-                cache: false,
-                beforeSend: function (settings) {
-                    settings.data[criteriaName] = settings.urlData.query;
+        element
+            .dropdown({
+                delay: {
+                    search: 250
+                },
+                minCharacters: 3,
+                forceSelection: false,
+                apiSettings: {
+                    dataType: 'JSON',
+                    cache: false,
+                    beforeSend: function beforeSend(settings) {
+                        /* eslint-disable-next-line no-param-reassign */
+                        settings.data[criteriaName] = settings.urlData.query;
+
+                        return settings;
+                    },
+                    onResponse: function onResponse(response) {
+                        return {
+                            success: true,
+                            results: response.map(function (item) {
+                                return {
+                                    name: item[choiceName],
+                                    value: item[choiceValue]
+                                };
+                            })
+                        };
+                    }
+                }
+            });
+
+        if (autocompleteValue.split(',')
+            .filter(String).length > 0) {
+            var menuElement = element
+                .find('div.menu');
+
+            menuElement.api({
+                on: 'now',
+                method: 'GET',
+                url: loadForEditUrl,
+                beforeSend: function beforeSend(settings) {
+                    /* eslint-disable-next-line no-param-reassign */
+                    settings.data[choiceValue] = autocompleteValue.split(',')
+                        .filter(String);
 
                     return settings;
                 },
-                onResponse: function(response) {
-                    return {
-                        success: true,
-                        results: response.map(function(item) {
-                            return {
-                                name: item[choiceName],
-                                value: item[choiceValue]
-                            };
-                        })
-                    };
+                onSuccess: function onSuccess(response) {
+                    response.forEach(function (item) {
+                        menuElement.append($('<div class="item" data-value="' + item[choiceValue] + '">' + item[choiceName] + '</div>'));
+                    });
                 }
-            }
-        });
-    });*/
+            });
+        }
+
+        window.setTimeout(function () {
+            element
+                .dropdown('set selected', element
+                    .find('input.autocomplete')
+                    .val()
+                    .split(',')
+                    .filter(String));
+        }, 5000);
+    });
+};
+
+var setupForms = function () {
+    $('#odiseo_sylius_report_dataFetcher_configuration .ui.toggle.checkbox').checkbox();
+    $('#odiseo_sylius_report_dataFetcher_configuration .ui.dropdown').dropdown();
+
+    setupAutocomplete();
 };
 
 (function ($) {
